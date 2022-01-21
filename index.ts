f8b9265413611a851fb5dd8140aedc4551d85d2b
Sam Winslow <sammywinslow@gmail.com>
2022-01-21 10:45:46 -0500

    extruded airspace (works with simple class C)

--- index.ts
+++ index.ts
@@ -4,28 +4,56 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { Theme } from "@here/harp-datasource-protocol"
-import { GeoCoordinates } from "@here/harp-geoutils"
-import { GeoJsonDataProvider, VectorTileDataSource } from "@here/harp-vectortile-datasource"
+import { Feature, FeatureCollection, Theme } from '@here/harp-datasource-protocol'
+import { GeoCoordinates } from '@here/harp-geoutils'
+import { GeoJsonDataProvider, VectorTileDataSource } from '@here/harp-vectortile-datasource'
+import { parseAltitude } from './utils'
 
-import { View } from "./View"
+import { View } from './View'
 
 const app = new View({
-  canvas: document.getElementById("map") as HTMLCanvasElement
+  canvas: document.getElementById('map') as HTMLCanvasElement
 })
 
 const mapView = app.mapView
 
-const airspaceDataPath = "resources/united_states.geojson"
+const airspaceDataPath = 'resources/united_states.geojson'
 
-async function getAirspace() {
+const addFeatureHeight = (feature: Feature): Feature => {
+  const { geometry, properties } = feature
+  if (geometry.type !== 'Polygon' || !properties.Top) {
+    return feature
+  }
+
+  const floorAltitude = parseAltitude(properties.Base ?? 'GND')
+  const ceilAltitude = parseAltitude(properties.Top)
+  return {
+    ...feature,
+    properties: {
+      ...feature.properties,
+      min_height: floorAltitude.agl,
+      height: ceilAltitude.agl,
+    }
+  }
+}
+
+const processFeatures = (features: FeatureCollection): FeatureCollection => {
+  const { features: collection } = features
+  return {
+    ...features,
+    features: collection.map(addFeatureHeight)
+  }
+}
+
+const getAirspace = async () => {
   const res = await fetch(airspaceDataPath)
-  const data = await res.json()
-  const dataProvider = new GeoJsonDataProvider("airspace", data)
+  const data = processFeatures(await res.json())
+  console.log(data)
+  const dataProvider = new GeoJsonDataProvider('airspace', data)
   const dataSource = new VectorTileDataSource({
-  dataProvider,
-  name: "airspace",
-  styleSetName: "geojson",
+    dataProvider,
+    name: 'airspace',
+    styleSetName: 'geojson',
   })
 
   await mapView.addDataSource(dataSource)
@@ -34,17 +62,21 @@ async function getAirspace() {
     styles: {
       geojson: [
         {
-          when: ["==", ["geometry-type"], "Point"],
-          technique: "circles",
+          when: ['==', ['geometry-type'], 'Point'],
+          technique: 'circles',
           renderOrder: 2,
-          color: "#FF0000",
+          color: '#FF0000',
           size: 15,
         },
         {
-          when: ["==", ["geometry-type"], "Polygon"],
-          technique: "extruded-polygon",
+          when: ['==', ['geometry-type'], 'Polygon'],
+          technique: 'extruded-polygon',
           renderOrder: 1,
-          color: "rgba(0,0,255,0.5)",
+          color: 'rgb(0,0,255)',
+          lineColor: 'rgb(0,0,128)',
+          opacity: 0.5,
+          transparent: true,
+          lineWidth: 1,
         },
       ],
     },
@@ -58,7 +90,7 @@ async function getAirspace() {
 mapView.resize(window.innerWidth, window.innerHeight)
 
 // react on resize events from the browser.
-window.addEventListener("resize", () => {
+window.addEventListener('resize', () => {
   mapView.resize(window.innerWidth, window.innerHeight)
 })
 
