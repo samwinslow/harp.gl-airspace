4a048c9b40a466383bdcda48fee30ab057cc15b7
samwinslow <sammywinslow@gmail.com>
2022-01-21 00:12:48 -0500

    parseAltitude conversions

--- utils/index.test.ts
+++ utils/index.test.ts
@@ -1,17 +1,58 @@
-import { getAltitudeType } from "./index"
+import { getAltitudeType, parseAltitude } from "./index"
 
 describe('getAltitudeType', () => {
-  it('should parse agl', () => {
+  it('parses agl', () => {
     expect(getAltitudeType(`2100 FT AGL`)).toEqual('agl')
     expect(getAltitudeType(`2100' AGL`)).toEqual('agl')
     expect(getAltitudeType(`2100 agl`)).toEqual('agl')
   })
-  it('should parse msl', () => {
-    expect(getAltitudeType(`2100 FT MSL`)).toEqual('msl')
+
+  it('parses msl', () => {
+    expect(getAltitudeType(`2100 FT AMSL`)).toEqual('msl')
     expect(getAltitudeType(`2100' MSL`)).toEqual('msl')
     expect(getAltitudeType(`2100 msl`)).toEqual('msl')
   })
-  it('should assume msl if no reference given', () => {
+
+  it('returns agl for 0 ground reference', () => {
+    expect(getAltitudeType(`GND`)).toEqual('agl')
+    expect(getAltitudeType(`gnd`)).toEqual('agl')
+  })
+
+  it('assumes msl if no reference given', () => {
     expect(getAltitudeType(`2100`)).toEqual('msl')
+  })
+})
+
+describe('parseAltitude', () => {
+  it('returns a well-formatted object', () => {
+    const alt = parseAltitude('1700 FT AMSL')
+    expect(alt._inferredType).toBe('msl')
+    expect(alt.agl).toBe(1700)
+    expect(alt.msl).toBe(1700)
+    expect(alt.toString()).toEqual('1700 MSL')
+  })
+
+  it('handles msl with elevation offset -> agl', () => {
+    const alt = parseAltitude('1700 FT AMSL', 250)
+    expect(alt._inferredType).toBe('msl')
+    expect(alt.agl).toBe(1450)
+    expect(alt.msl).toBe(1700)
+    expect(alt.toString()).toEqual('1700 MSL')
+  })
+
+  it('handles agl with elevation offset -> msl', () => {
+    const alt = parseAltitude('1450 FT AGL', 250)
+    expect(alt._inferredType).toBe('agl')
+    expect(alt.agl).toBe(1450)
+    expect(alt.msl).toBe(1700)
+    expect(alt.toString()).toEqual('1450 AGL')
+  })
+
+  it('handles gnd with elevation offset', () => {
+    const alt = parseAltitude('GND', 250)
+    expect(alt._inferredType).toBe('agl')
+    expect(alt.agl).toBe(0)
+    expect(alt.msl).toBe(250)
+    expect(alt.toString()).toEqual('0 AGL')
   })
 })
--- utils/index.ts
+++ utils/index.ts
@@ -2,34 +2,36 @@ interface Altitude {
   msl: number
   agl: number
   toString: () => string
+  _inferredType: 'msl' | 'agl'
 }
 
-const parseInt_safe = (input: string): number | null => {
+const parseInt_safe = (input: string): number => {
   const output = parseInt(input)
   if (!isNaN(output)) return output
-  return null
+  return 0
 }
 
 export const getAltitudeType = (input: string): 'msl' | 'agl' => {
-  if (/agl/i.test(input)) return 'agl'
+  if (/agl|gnd/i.test(input)) return 'agl'
   return 'msl'
 }
 
-export const parseAltitude = (input: string, elevation: number): Altitude | null => {
+export const parseAltitude = (input: string, elevation: number = 0): Altitude | null => {
   const feet = parseInt_safe(input)
-  if (feet == null) return null
 
   const output = {} as Altitude
   const altitudeType = getAltitudeType(input)
 
   if (altitudeType === 'agl') {
+    output._inferredType = 'agl'
     output.agl = feet
     output.msl = feet + elevation
-  } else { // msl
+  } else {
+    output._inferredType = 'msl'
     output.agl = feet - elevation
     output.msl = feet
   }
 
-  output.toString = () => `${output} ${altitudeType.toUpperCase()}`
+  output.toString = () => `${feet} ${altitudeType.toUpperCase()}`
   return output
 }
